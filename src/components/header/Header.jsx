560d81aed93d44338b38e2948a1005dbf6e80c9a
Mustafa Hasan <mustafahasan@Mustafas-Air.ht.home>
2022-08-23 01:33:40 -0400

    added images and buttons

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/image.png'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header> 
+      <div className="container header_container"></div>
+      <h5>Hello I'm</h5>
+      <h1>Mustafa Hasan</h1>
+      <h5 className="text-light">Computer Science Student</h5>
+      <CTA />
+
+      <div className='me'>
+        <img src = {ME} alt = "me" height= 'auto' width = '10%'/>
+
+      </div>
+    </header>
   )
 }
 
