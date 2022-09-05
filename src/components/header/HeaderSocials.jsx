a3c62c1631fa128b973e7e91c66b8ed0671136f3
Mustafa Hasan <mustafahasan@Mustafas-Air.ht.home>
2022-09-05 19:20:10 -0400

    fixed linkedin url

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -4,7 +4,7 @@ import{BsGithub} from 'react-icons/bs'
 const HeaderSocials = () => {
   return (
     <div className = 'header_socials'>
-        <a href = "www.linkedin.com/in/mustafa-hasan1702" target='_blank'><BsLinkedin/></a>
+        <a href = "https://www.linkedin.com/in/mustafa-hasan-525473237/" target='_blank'><BsLinkedin/></a>
         <a href = "https://github.com/mustafah1702" target="_blank"><BsGithub/></a>
     </div>
   )
