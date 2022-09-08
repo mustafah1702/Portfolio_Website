e95b057d78ecc1f50003aba08a888e1619107eb5
Mustafa Hasan <mustafahasan@Mustafas-Air.ht.home>
2022-09-08 02:20:17 -0400

    about edited:

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
+import { icons } from 'react-icons'
 import "./about.css"
+import {FiAward} from 'react-icons/fi'
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className='container about_container'>
+        <div className='about_me'> 
+        <div className='about_me-image'>
+          <img src="" alt="About Image" />
+        </div>
+        </div>
+        <div className='about_content'>
+          <div className='about_cards'>
+            <article className='about_card'>
+              <FiAward className='about_icon'/>
+              <h5>Experience</h5>
+              <small> 3+ Years Working</small>
+            </article>
+
+            <article className='about_card'>
+              <FiAward className='about_icon'/>
+              <h5>Experience</h5>
+              <small> 3+ Years Working</small>
+            </article>
+
+            <article className='about_card'>
+              <FiAward className='about_icon'/>
+              <h5>Experience</h5>
+              <small> 3+ Years Working</small>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
