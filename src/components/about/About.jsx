cd0c7a244317284f6ea3ecb41c5a11939ac585e5
Mustafa Hasan <mustafahasan@Mustafas-Air.ht.home>
2022-09-08 13:25:58 -0400

    about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { icons } from 'react-icons'
 import "./about.css"
 import {FiAward} from 'react-icons/fi'
+import {BiBookBookmark} from 'react-icons/bi'
+import {MdComputer} from 'react-icons/md'
 
 const About = () => {
   return (
@@ -24,17 +26,23 @@ const About = () => {
             </article>
 
             <article className='about_card'>
-              <FiAward className='about_icon'/>
-              <h5>Experience</h5>
-              <small> 3+ Years Working</small>
+              <BiBookBookmark className='about_icon'/>
+              <h5>Education</h5>
+              <small> Toronto Metropolitan University </small>
+              <h6>2022 - Current</h6>
+              <small> University of Toronto </small>
+              <h6>2020 - 2022</h6>
             </article>
 
             <article className='about_card'>
-              <FiAward className='about_icon'/>
-              <h5>Experience</h5>
-              <small> 3+ Years Working</small>
+              <MdComputer className='about_icon'/>
+              <h5>Technical Competencies</h5>
+              <p><small> Programming Languages Python, Java, RISC-V Assembly,  HTML, CSS, Javascript, SQL </small></p>
+              <p><small> Git, GitHub, Jira, Firebase (NoSQL) database</small></p>
+              <small> Microsoft Office: Word, Excel (Pivot Tables, Gantt Chart, Data Manipulation), PowerPoint, Access</small>
             </article>
           </div>
+          <a href='#contact' className='btn btn-primary'>Let's Talk </a>
         </div>
       </div>
     </section>
